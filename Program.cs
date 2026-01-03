95397679abb4e1489fd2525e3c8c6431e16bb2fc
goyallucky <luckygoyal221982@gmail.com>
2026-01-03 11:52:00 +0530

    [Lucky Goyal] solved Week2 Review problem

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Student student = new Student("lucky Goyal", 811, 78);
+		University university = new University();
+
+		university.EnrollStudent(student);
+
+		using (AcademicEngine engine = new AcademicEngine())
+		{
+			engine.EvaluateStudent(student);
+			Console.WriteLine(student.GetStudentDetails());
+		}
+
+	}
+}
